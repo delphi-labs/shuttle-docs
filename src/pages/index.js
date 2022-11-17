c31004c2edb27a4d8f24100b32ef19516e2933a5
Dino De La O <dinodelao@proton.me>
2022-11-17 10:37:54 -0600

    icon changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/getting-started/installation">
-            Install Now 🚀
+            Install Now
           </Link>
         </div>
       </div>
